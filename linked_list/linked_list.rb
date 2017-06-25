cb0a5ff0aa48a4e1bf052a5d57acc3fbb87d6314
codonculous <feidameng01@gmail.com>
2017-06-24 21:28:35 -0400

    created append method for Linked_list class

--- linked_list/linked_list.rb
+++ linked_list/linked_list.rb
@@ -5,3 +5,26 @@ class Node
     @next = nil
   end
 end
+
+class Linked_list
+  attr_accessor :root, :size
+  def initialize
+    @root = nil
+    @size = 0
+  end
+
+  def append(item)
+    if @size == 0
+      @root = item
+    else
+      current = @root
+      while current.nextn
+        current = current.nextn
+      end
+      current.nextn = item
+    end
+    @size += 1
+  end
+
+  
+end

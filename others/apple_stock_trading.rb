192ddf39b2dc3796273752a2d9025a4868608f9b
codonculous <feidameng01@gmail.com>
2017-06-25 12:01:09 -0400

    debugging the logic, make sure the max_profit not include the difference between the max price eariler and later min price

--- others/apple_stock_trading.rb
+++ others/apple_stock_trading.rb
@@ -6,24 +6,37 @@
 # Write an efficient function that takes stock_prices_yesterday and returns the best
 # profit I could have made from 1 purchase and 1 sale of 1 Apple stock yesterday.
 def max_profit(price)
-  max = price[0]
-  min = price[0]
-  price.each do |p|
-    if p > 0
-      max = p if p > max
-      min = p if p < min
+  max = 0
+  min = 0
+  max_profit = 0
+  price.each_index do |i|
+    if price[i] >= 0
+
+      if price[i] < price[min]
+        min = i
+      end
+
+      if price[i] - price[min] > max_profit
+        max_profit = price[i] - price[min]
+        max = i
+      end
+
     end
   end
 
-  return [min,max,max - min]
+  min = price.index(price[max] -max_profit)
+  return [price[min],price[max],max_profit]
 
 end
 
 # test
-p1 = [120,110,99,111,123,144,155,139,114,121]
-p2 = [0,120,120,133,93,144,144]
-
-puts max_profit(p1)
-puts "--------------"
-puts max_profit(p2)
+p1 = [120,110,99,111,123,144,155,139,114,121,89]
+p2 = [150,120,120,133,93,144,144,20]
+p3 = [100,45,120,105,110,43,100,50,113,30,100]
+#
+puts "min : #{max_profit(p1)[0]}, max : #{max_profit(p1)[1]}, max_profit : #{max_profit(p1)[2]}"
+# # puts "--------------"
+puts "min : #{max_profit(p2)[0]}, max : #{max_profit(p2)[1]}, max_profit : #{max_profit(p2)[2]}"
+# puts "--------------"
+puts "min : #{max_profit(p3)[0]}, max : #{max_profit(p3)[1]}, max_profit : #{max_profit(p3)[2]}"
 puts "--------------"
